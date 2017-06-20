cf35bc8bdd2c91db163432707136fd33b6b10911
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 16:10:52 +0000

    integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,8 +31,8 @@ end
   
   puts "5 skills created"
   
-  9.times do |portfolio_items|
-    portfolio.create!(
+  9.times do |portfolio_item|
+    Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}",
       subtitle: "my great service",
       body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
