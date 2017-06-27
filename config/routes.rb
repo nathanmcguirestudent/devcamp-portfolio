33c15d1cd4063152f0f88196c51b1d847532ac16
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 14:24:46 +0000

    Override the Rails Resources Routes for the Show Action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios
-
-  get 'about', to: "pages#about"
+  resources :portfolios, except: [:show]
+  get "portfolios/:id", to: "portfolios#show", as: "portfolio_show"
+  
+  get "about-me", to: "pages#about"
+  
   get "contact", to: "pages#contact"
 
   resources :blogs
