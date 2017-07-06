fc71883fa0b0905cfa836c85e309937a574b16f0
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 14:18:03 +0000

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
+  
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   
@@ -14,4 +16,6 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
     self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
   end
+  
+  
 end
